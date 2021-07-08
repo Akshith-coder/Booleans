182e73ae31c491a68035abf82b97cb3fce57711a
AkshithKachaka1 <>
2021-07-08 16:45:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+#Definition of Booleans
+
+'''''
+Booleans are used to see if a particular value is True or False
+
+When you compare two values, the expression will get evaluated and you will get Boolean answer of True or False from Python
+
+'''
+
+#Using Booleans
+
+''''
+Most Values are True
+
+- Any string is True, except empty strings.
+
+- Any number is True, except 0.
+
+- Any list, tuple, set, and dictionary are True, except empty ones.
+
+- By default, anything is evaluated to True if the value has any content
+'''
+
+# Operators
+
+''''
+The used operators are:
+
+Less than - <
+
+Greater than - >
+
+equal to - ==
+
+not equal to - !=
+
+Less than or equal to - <=
+
+Greater than or equal to - >=
+'''
+
+x = 10
+print(x < 1)
+
+
+a = 20
+print(a != 20)
+
+
+b = 25
+print(b >= 25)
